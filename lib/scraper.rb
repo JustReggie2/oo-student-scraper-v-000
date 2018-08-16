3fae6ac4beefddd3bc588a44b213fc8f7e280f8b
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-16 21:58:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-# binding.pry
+binding.pry
     s_attr = {}
     doc.css(".social-icon_container a").collect do |s_links|
       case s_links.css("href").text
